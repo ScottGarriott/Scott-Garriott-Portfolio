468e50d8d66ac18346442546c1f3cb75a0d52538
Scott Garriott <garriottscott@gmail.com>
2022-02-09 15:45:36 -0800

    redesiging website to be more simple, using html and css instead of react

--- src/menu/Menu.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Menu() {
-    return <h2>This is a menu</h2>
-}
-
-export default Menu
--- src/title-graphic/TitleGraphic.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BlueSky from "../images/blue-sky-image.jpg"
-
-function TitleGraphic() {
-    return <img src={BlueSky} alt="A blue sky with sparse clouds" />
-}
-
-export default TitleGraphic
